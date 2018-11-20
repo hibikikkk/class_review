d79f283174d60349cf579573d73d086f84f55729
hibikikkk <kyou9712@gmail.com>
2018-11-20 14:40:06 +0900

    b-2完成

--- execution.py
+++ execution.py
@@ -1,1 +1,11 @@
 from customer import Customer
+
+if __name__ == "__main__":
+    ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+    print(ken.age)
+
+    tom = Customer(first_name="Tom", family_name="Ford", age=57)
+    print(tom.age)
+
+    ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+    print(ieyasu.age)
