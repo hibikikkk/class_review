8f6a184613f1d9360850afb2ea2b8b47913355e4
hibikikkk <kyou9712@gmail.com>
2018-11-20 15:33:43 +0900

    b-8完成

--- customer.py
+++ customer.py
@@ -38,3 +38,7 @@ class Customer:
         return f"{user_full_name}   {self.age}  {user_payment}"
 
     def info_format_split_pipe(self):
+        user_full_name = self.full_name()
+        user_payment = self.entry_fee()
+
+        return f"{user_full_name}|{self.age}|{user_payment}"
--- execution_b-8.py
+++ execution_b-8.py
@@ -1,3 +1,10 @@
 from customer import Customer
 
 if __name__ == "__main__":
+    ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+    tom = Customer(first_name="Tom", family_name="Ford", age=57)
+    ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+
+    print(ken.info_format_split_pipe())
+    print(tom.info_format_split_pipe())
+    print(ieyasu.info_format_split_pipe())
