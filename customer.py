7037ff61e10b6fc5396727ef5e61e5c36601c14f
hibikikkk <kyou9712@gmail.com>
2018-11-20 14:36:57 +0900

    b-2途中、実行用ファイル追加

--- customer.py
+++ customer.py
@@ -1,16 +1,12 @@
 class Customer:
 
-    def __init__(self, first_name, family_name):
+    def __init__(self, first_name, family_name, age):
         self.first_name = first_name
         self.family_name = family_name
+        self.age = age
 
     def full_name(self):
         print(self.first_name + " " + self.family_name)
 
 
-if __name__ == "__main__":
-    ken = Customer(first_name="Ken", family_name="Tanaka")
-    ken.full_name()
 
-    tom = Customer(first_name="Tom", family_name="Ford")
-    tom.full_name()
